a8978f5d7654920cda1a360c4c7e0a990e3285f7
Lingdong Huang <lingdong0618@hotmail.com>
2017-01-12 12:56:08 +0800

    Indent using spaces

--- charsimilarity.py
+++ charsimilarity.py
@@ -4,36 +4,36 @@ import json
 
 
 def charsim():
-	result = {}
-	cimgs = mo.cimgs
-	C = mo.C
+    result = {}
+    cimgs = mo.cimgs
+    C = mo.C
 
-	for i in range(0,len(cimgs)):
-		print C[i]
-		scores = []
-		px1 = cimgs[i].load()
-		for j in range(0,len(cimgs)):		
-			if i != j:
-				score = 0
-				px2 = cimgs[j].load()
-				for x in range(0,cimgs[i].size[0]):
-					for y in range(0,cimgs[i].size[1]):
-						if x >= cimgs[j].size[0] or y >= cimgs[j].size[1]:
-							score -= 1
-						if px1[x,y][:3] == px2[x,y][:3]:
-							score += 1
-						else:
-							score -= 1
-				scores.append((C[j],score))
-		ns = mo.normalize(scores)
-		result[C[i]] = {}
-		for n in ns:
-			result[C[i]][n[0]] = n[1] 
+    for i in range(0,len(cimgs)):
+        print C[i]
+        scores = []
+        px1 = cimgs[i].load()
+        for j in range(0,len(cimgs)):       
+            if i != j:
+                score = 0
+                px2 = cimgs[j].load()
+                for x in range(0,cimgs[i].size[0]):
+                    for y in range(0,cimgs[i].size[1]):
+                        if x >= cimgs[j].size[0] or y >= cimgs[j].size[1]:
+                            score -= 1
+                        if px1[x,y][:3] == px2[x,y][:3]:
+                            score += 1
+                        else:
+                            score -= 1
+                scores.append((C[j],score))
+        ns = mo.normalize(scores)
+        result[C[i]] = {}
+        for n in ns:
+            result[C[i]][n[0]] = n[1] 
 
-	return result
+    return result
 
 if __name__ == "__main__":
- 	mo.makeglyphs()
-	js = json.dumps(charsim(), sort_keys=True)
-	f1 = open("similarity.json","w")
-	f1.write(js)
+    mo.makeglyphs()
+    js = json.dumps(charsim(), sort_keys=True)
+    f1 = open("similarity.json","w")
+    f1.write(js)
